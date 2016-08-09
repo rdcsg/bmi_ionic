6e13f9e8dbc31ae144c01ba98e73fca4f32e85b4
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-08-09 08:19:22 +0200

    add calculation functionality

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,16 @@
 angular.module('starter.controllers', [])
 
-.controller('CalculatorController', function () {
-
-})
+.controller('CalculatorController', function ($scope) {
+  $scope.data= {};
+  $scope.calculateBMI = function () {
+    var  person = new Person({
+        weight: $scope.data.weight,
+        height: $scope.data.height
+    });
+    person.calculate_bmi_met();
+    $scope.person = person;
+  };
+});
 
 .controller('AboutController', function () {
 
